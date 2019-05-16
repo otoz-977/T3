b987eb20dbece4a73a7594541ecb1c9934dd730f
otoniel baeza <otoniel.baeza@gmail.com>
2019-05-16 12:02:42 -0600

    final-pregunta 3

---
+++ T3/T3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T3
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            string nombre;
+            string apellido;
+            
+            Console.WriteLine("hey, como estas? me podrias decir tu nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("hola {0}, me podrias decir tu apellido",nombre);
+            apellido = Console.ReadLine();
+
+            CheckL(nombre);
+            CheckL(apellido);
+
+            Console.WriteLine("{0} {1}, Stop! You have violated the Law! Pay the court a fine or serve your sentance. ", nombre,apellido);
+
+        }
+
+
+
+
+        static void CheckL(string str)
+        {
+            int length = str.Length;
+
+            if (length <= 2)
+            {
+                throw new System.ArgumentException("nombre/apellido no valido");
+            }
+        }
+
+
+    }
+}
